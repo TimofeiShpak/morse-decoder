656e8b2d566ec9c3fb7de40737e3eef7cc7d15c1
TimofeiShpak <60565887+TimofeiShpak@users.noreply.github.com>
2020-03-08 19:38:34 +0400

    init: begin task morse-decoder

--- src/index.js
+++ src/index.js
@@ -38,7 +38,40 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let array = expr.split('');
+    let arr = [];
+    let index = 0;
+    arr[0]=array[0];
+    let arrs = [];
+    let result = '';
+    for(let i = 1; i<array.length; i++){
+        if(i%10!=0){arr[index]+=array[i];}
+        if(i%10==0 && i!=0){index++;}
+        if(i%10==0){arr[index]=array[i];}
+    }
+    for(let i = 0 ; i<arr.length; i++){
+        let letter = arr[i].split('');
+        let char='';
+        if(letter[0]!='*'){
+            let letters = [];
+            letters[0]=letter[0];
+            let index = 0;
+            for(let i = 1; i<letter.length; i++){
+                if(i%2!=0){letters[index]+=letter[i];}
+                if(i%2==0 && i!=0){index++;}
+                if(i%2==0){letters[index]=letter[i];}
+            }
+            for(let i=0; i<letters.length; i++){
+                if(letters[i]=='11')char+='-';
+                if(letters[i]=='10')char+='.';
+            }
+            result+=MORSE_TABLE[char];
+        }
+
+        if(letter[0]=='*')result+=' ';
+        
+    }
+    return result;
 }
 
 module.exports = {
